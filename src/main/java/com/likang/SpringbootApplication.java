0634e2c2a8e6cd7fdcff35d8a53ce97fb16d7abe
李康 <li.kang@trs.com.cn>
2019-11-25 03:59:37 +0000

    更新 SpringbootApplication.java

--- src/main/java/com/likang/SpringbootApplication.java
+++ src/main/java/com/likang/SpringbootApplication.java
@@ -8,6 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class,args);
-        System.out.println("aaa");
+        System.out.println("bbb");
     }
 }
