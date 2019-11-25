db25fe6dd1779314091b9b9796c570626f2afebe
16619921167 <qqqq602444416@163.com>
2019-11-25 15:01:42 +0800

    Default Changelist

--- src/main/java/com/likang/SpringbootApplication.java
+++ src/main/java/com/likang/SpringbootApplication.java
@@ -8,6 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class,args);
-        System.out.println("1");
+        System.out.println("11");
     }
 }
