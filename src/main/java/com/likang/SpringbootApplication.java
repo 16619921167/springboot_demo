4674726dd7d7356836377bd89d8d662b121eed66
16619921167 <qqqq602444416@163.com>
2019-12-15 11:34:10 +0800

    Merge branch 'master' of https://github.com/16619921167/springboot_demo
    # Conflicts:
    #	src/main/java/com/likang/SpringbootApplication.java

--- src/main/java/com/likang/SpringbootApplication.java
+++ src/main/java/com/likang/SpringbootApplication.java
@@ -8,8 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class,args);
-
-        System.out.println("2");
-
+        System.out.println("2111111111");
     }
 }
