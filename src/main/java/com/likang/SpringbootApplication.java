242b463a1c56a4afbace3dba83026a48a2067a20
16619921167 <qqqq602444416@163.com>
2019-12-15 11:51:26 +0800

    bbb

--- src/main/java/com/likang/SpringbootApplication.java
+++ src/main/java/com/likang/SpringbootApplication.java
@@ -8,6 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class,args);
-        System.out.println("111111111144444444444444");
+        System.out.println("111111111144444444444444111111111");
     }
 }
