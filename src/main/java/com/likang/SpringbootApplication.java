a2bee97d729652febdcdb4e1dce833d431d98bbb
16619921167 <47733481+16619921167@users.noreply.github.com>
2019-12-23 20:37:10 +0800

    Update SpringbootApplication.java

--- src/main/java/com/likang/SpringbootApplication.java
+++ src/main/java/com/likang/SpringbootApplication.java
@@ -8,6 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class,args);
-        System.out.println("111221111");
+        System.out.println("1112211111111111111111111111111");
     }
 }
