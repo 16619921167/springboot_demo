d41054963a66de18fe554a4f8c129556f81611d3
16619921167 <47733481+16619921167@users.noreply.github.com>
2020-01-01 17:09:14 +0800

    Update SpringbootApplication.java

--- src/main/java/com/likang/SpringbootApplication.java
+++ src/main/java/com/likang/SpringbootApplication.java
@@ -9,7 +9,7 @@ public class SpringbootApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class,args);
         aaaaaaaaaaaa
-        System.out.println("11122111111111111111111111111114444444444444qqqsss11111111111eeee");
+        System.out.println("11122111111111111111111111111114444444444444qqqsss11111111111eeeeffff");
         System.out.println();
 
     }
